2256e1de743032ddaef3ada1b6dfe52fd1e8fed1
Joshua Langer <joshua.r.langer@gmail.com>
2024-08-12 06:54:28 -0400

    Added new palette const and widgets

--- src/theme/mod.rs
+++ src/theme/mod.rs
@@ -1,5 +1,9 @@
+pub mod palette;
 mod widgets;
 
 pub mod prelude {
-    pub use super::widgets::Containers as _;
+    pub use super::{
+        //palette as ui_palette,
+        widgets::{Containers as _, Widgets as _},
+    };
 }
--- src/theme/widgets.rs
+++ src/theme/widgets.rs
@@ -1,7 +1,13 @@
 use bevy::{ecs::system::EntityCommands, prelude::*, ui::Val::*};
 
+use super::palette::LABEL_TEXT;
+
 pub trait Containers {
     fn ui_root(&mut self) -> EntityCommands;
+}
+
+pub trait Widgets {
+    fn label(&mut self, text: impl Into<String>) -> EntityCommands;
 }
 
 impl Containers for Commands<'_, '_> {
@@ -22,5 +28,42 @@ impl Containers for Commands<'_, '_> {
                 ..Default::default()
             },
         ))
+    }
+}
+
+impl<T: Spawn> Widgets for T {
+    fn label(&mut self, text: impl Into<String>) -> EntityCommands {
+        let entity = self.spawn((
+            Name::new("Label"),
+            TextBundle::from_section(
+                text,
+                TextStyle {
+                    font_size: 24.0,
+                    color: LABEL_TEXT,
+                    ..default()
+                },
+            )
+            .with_style(Style {
+                width: Px(500.0),
+                ..Default::default()
+            }),
+        ));
+        entity
+    }
+}
+
+trait Spawn {
+    fn spawn<B: Bundle>(&mut self, bundle: B) -> EntityCommands;
+}
+
+impl Spawn for Commands<'_, '_> {
+    fn spawn<B: Bundle>(&mut self, bundle: B) -> EntityCommands {
+        self.spawn(bundle)
+    }
+}
+
+impl Spawn for ChildBuilder<'_> {
+    fn spawn<B: Bundle>(&mut self, bundle: B) -> EntityCommands {
+        self.spawn(bundle)
     }
 }
